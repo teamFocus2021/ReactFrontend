65f62f96bf325805f192da5165030503752deec6
Deokk <deokk1112@gmail.com>
2021-01-12 17:18:00 +0900

    Upgrade Uploader to handle only one item

--- src/Uploader.js
+++ src/Uploader.js
@@ -5,35 +5,43 @@ import 'react-dropzone-uploader/dist/styles.css'
 import "./dropbox.css"
 
   const Uploader = () => {
+    
     const getUploadParams = ({ meta }) => {
       const url = 'https://httpbin.org/post'
       return { 
         url, meta: { fileUrl: `${url}/${encodeURIComponent(meta.name)}` }
       }
     }
-  
-    // called every time a file's `status` changes
-    const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
-  
-    // receives array of files that are done uploading when submit button is clicked
+    
+    const handleChangeStatus = ({ meta, remove }, status) => {
+      console.log(status, meta)
+    }
+    
     const handleSubmit = (files, allFiles) => {
       console.log(files.map(f => f.meta))
       allFiles.forEach(f => f.remove())
     }
 
     return (
-      <Dropzone
-        getUploadParams={getUploadParams}
-        onChangeStatus={handleChangeStatus}
-        onSubmit={handleSubmit}
-        accept="video/*"
-        inputContent={(files, extra) => (extra.reject ? 'video files only' : 'Drag Files')}
-        styles={{
-          dropzoneReject: { borderColor: 'red', backgroundColor: '#DAA' },
-          inputLabel: (files, extra) => (extra.reject ? { color: 'red' } : {}),
-        }}
-      />
+      <React.Fragment>
+        <Dropzone
+          getUploadParams={getUploadParams}
+          onChangeStatus={handleChangeStatus}
+          maxFiles={1}
+          multiple={false}
+          canCancel={false}
+          onSubmit={handleSubmit}
+          accept="video/*"
+          inputContent={(files, extra) => (extra.reject ? 'Only Video File!' : 'Drop or Browse A Video File')}
+          styles={{
+            dropzoneReject: { borderColor: 'red', backgroundColor: '#DAA' },
+            inputLabel: (files, extra) => (extra.reject ? { color: 'red' } : {}),
+          }}
+        />
+      </React.Fragment>
     )
   }
 
 export default Uploader;
+
+// 참고: https://react-dropzone-uploader.js.org/
